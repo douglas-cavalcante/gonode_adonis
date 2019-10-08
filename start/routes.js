6094e35fb7792e13f1dff18dbbb214f7af4f41bc
douglas-cavalcante <henriquedouglas2803@gmail.com>
2019-10-07 23:07:16 -0300

    Valida rotas da aplicação

--- start/routes.js
+++ start/routes.js
@@ -1,18 +1,26 @@
-'use strict';
+'use strict'
 
-const Route = use('Route');
+const Route = use('Route')
 
-Route.post('users', 'UserController.store').validator('User');
-Route.post('sessions', 'SessionController.store');
-Route.post('passwords', 'ForgotPasswordController.store');
+Route.post('users', 'UserController.store').validator('User')
+Route.post('sessions', 'SessionController.store').validator('Session')
+Route.post('passwords', 'ForgotPasswordController.store').validator(
+  'ForgotPassword'
+)
+Route.put('passwords', 'ForgotPasswordController.update').validator(
+  'ResetPassword'
+)
 
-Route.put('passwords', 'ForgotPasswordController.update');
-
-Route.get('files/:id', 'FileController.show');
+Route.get('files/:id', 'FileController.show')
 
 Route.group(() => {
-  Route.post('files', 'FileController.store');
+  Route.post('files', 'FileController.store')
 
-  Route.resource('projects', 'ProjectController').apiOnly();
-  Route.resource('projects.tasks', 'TaskController').apiOnly();
-}).middleware(['auth']);
+  Route.resource('projects', 'ProjectController')
+    .apiOnly()
+    .validator(new Map([[['projects.store'], ['Project']]]))
+
+  Route.resource('projects.tasks', 'TaskController')
+    .apiOnly()
+    .validator(new Map([[['projects.tasks.store'], ['Task']]]))
+}).middleware(['auth'])
