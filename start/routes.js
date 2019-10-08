17aeb6854eabf730cfcf5d089a9aa67b803373ed
douglas-cavalcante <henriquedouglas2803@gmail.com>
2019-10-07 22:43:42 -0300

    Adiciona validacao e tratamento de exceptions

--- start/routes.js
+++ start/routes.js
@@ -1,18 +1,18 @@
-'use strict'
+'use strict';
 
-const Route = use('Route')
+const Route = use('Route');
 
-Route.post('users', 'UserController.store')
-Route.post('sessions', 'SessionController.store')
-Route.post('passwords', 'ForgotPasswordController.store')
+Route.post('users', 'UserController.store').validator('User');
+Route.post('sessions', 'SessionController.store');
+Route.post('passwords', 'ForgotPasswordController.store');
 
-Route.put('passwords', 'ForgotPasswordController.update')
+Route.put('passwords', 'ForgotPasswordController.update');
 
-Route.get('files/:id', 'FileController.show')
+Route.get('files/:id', 'FileController.show');
 
 Route.group(() => {
-  Route.post('files', 'FileController.store')
+  Route.post('files', 'FileController.store');
 
-  Route.resource('projects', 'ProjectController').apiOnly()
-  Route.resource('projects.tasks', 'TaskController').apiOnly()
-}).middleware(['auth'])
+  Route.resource('projects', 'ProjectController').apiOnly();
+  Route.resource('projects.tasks', 'TaskController').apiOnly();
+}).middleware(['auth']);
